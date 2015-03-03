ae6ddd2bdc9e30facb74321767dc4733b39d8408
Asit <akasit@me.com>
2015-03-03 22:40:07 +0900

    PPS_TO_MRPSのtypoを修正

--- motor_unit/motor_unit.h
+++ motor_unit/motor_unit.h
@@ -10,7 +10,7 @@
 
 /****************************************/
 #define	F_SAMPLE	50
-#define	PPS_TO_MRPS	281	/* PulsePerSample to MilradianPerSec */
+#define	PPS_TO_MRPS	218	/* PulsePerSample to MilradianPerSec */
 						/* (6283 * F_SAMPLE) / ENCODER_PPR */ //77
 
 /****************************************/
